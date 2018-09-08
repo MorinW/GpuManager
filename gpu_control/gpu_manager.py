da830ba8e131ef6e9ae98c6191767debd82a7747
tf_fishbone <tf@fishbone>
2018-09-08 20:52:10 +0800

    publish 1.2.1, to add support keras

--- gpu_control/gpu_manager.py
+++ gpu_control/gpu_manager.py
@@ -2,7 +2,7 @@
 __all__ = ["GpuManager", "__author__", "__version__"]
 
 __author__ = "Fishbone"
-__version__ = "1.2.0"
+__version__ = "1.2.1"
 
 from gpu_control.gpu_utils import Linux_Gpu
 import platform

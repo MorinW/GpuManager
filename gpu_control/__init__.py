c1aafb780d680efcc9b9aaab23b332d266ed0576
tf_fishbone <tf@fishbone>
2018-09-11 14:40:51 +0800

    optimize logic

--- gpu_control/__init__.py
+++ gpu_control/__init__.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
 
+from gpu_control import gpu_manager
+from gpu_control import framework_setting
 
-__all__ = ["gpu_manager"]
+
+__all__ = ["gpu_manager", "framework_setting"]
